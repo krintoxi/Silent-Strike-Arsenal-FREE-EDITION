0eca635f276c81c090c6a409eaa737c0ef063efc
(Krintoxi) Leo Edge <shimmynetwork@gmail.com>
2022-11-08 21:46:14 -0500

    Update and addition of :alien: Commands

---
+++ scripts/commands.py
@@ -0,0 +1,80 @@
+print("""
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠔⠊⠀⠀⠀⢀⠀⣀⠈⠒⠤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠊⠁⠀⠀⠀⠈⠁⠈⠉⠉⠁⠀⠀⠈⠢⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠁⠀⠀⠀⠀⠀⣠⣾⢶⣶⣶⣯⡢⡀⠀⠀⠀⠈⠓⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠁⠀⠀⠀⠀⠀⣠⣾⣿⡇⡏⠉⣾⣿⡻⢮⡀⠀⠀⠀⠀⠀⠱⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠋⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⠫⠉⢭⣾⣿⣾⡄⠙⢦⣄⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣄⠀⢀⣠⡾⢟⣛⣯⠥⠤⠀⠒⠒⠒⠒⠶⠦⠭⣝⣒⡚⠷⣤⡀⠀⢠⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⢾⡵⣞⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⣿⣶⡿⠒⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡷⢜⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣶⠿⢋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠂⠀⢸⠋⠉⠁⢒⠒⠚⢻⠒⠉⠉⠉⢡⡇⠀⠀⢨⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠀⠇⢰⠀⡆⣀⣀⣀⡀⠁⡕⠘⡆⢀⣀⣄⣀⡣⠀⠀⠈⡄⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠜⢀⠈⡦⢾⠛⣻⣿⣿⠁⠙⠀⢩⢙⣿⣯⡏⠻⢲⠀⢠⠉⢹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⠎⠀⣿⠘⠀⠻⠙⠃⠀⠀⠀⠀⠎⠛⠙⠀⢀⣼⠀⠈⠂⠈⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣄⣸⠀⠀⠀⠀⡏⢢⠀⠀⠀⠀⠀⠀⠀⠀⠪⣂⠀⢀⢾⢿⠀⠀⠀⠀⣇⣠⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣼⣿⠀⠀⠀⡀⣿⣤⣷⣀⠀⠀⠀⠀⠀⠀⠀⠈⢀⣰⣇⣸⠀⠀⠀⠀⢹⣯⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠙⣻⣿⣿⠀⡆⠀⡇⣿⠇⠈⠙⣿⡖⠤⣀⣀⡠⠴⣾⠛⠉⠀⡾⢰⠀⠁⠀⣾⣿⣟⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠚⢋⣿⣿⢀⢰⠀⣷⠸⠀⠀⣰⢻⣟⠲⠤⠤⢴⣞⡿⢳⠀⠀⣙⣼⠀⡇⣴⣿⣿⡟⠓⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠠⠾⠿⣿⣿⣾⣄⠸⣶⠀⢠⣏⠈⢿⣷⠀⠀⢽⣿⠇⠘⣇⠀⢡⣃⣴⣷⣿⣿⡿⠿⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠴⣻⣿⠿⣿⣿⣿⣆⣸⣿⡦⣄⠉⠀⠀⢉⣀⡴⣾⣿⣠⣟⣿⣿⡿⠿⣟⠳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠙⣔⢍⡛⠧⣀⡈⠉⠭⠷⠦⣄⠀⣹⣿⠴⠿⠯⠭⠍⠀⠉⠉⠀⠀⠀⢰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠈⡗⠪⣑⠢⣍⠓⠦⣄⡀⠀⠁⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡀⠀⠀⡹⠀⠀⠉⠒⢍⠒⢤⣉⠛⠲⢦⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⢀⠇⠀⠀⠀⠀⠀⠑⢆⠈⠙⠒⠶⣤⣈⡉⠙⠛⠲⠶⢶⣤⣤⣀⣀⡀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⡌⠀⠀⡼⠀⠀⠀⠀⠀⠀⠀⠀⠳⡀⠀⠀⠀⠉⠙⠓⠶⢦⣤⣀⡀⠈⠉⠉⠙⢻⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣄⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠙⠓⠲⠦⣾⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢠⠃⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⠀⣤⣈⣣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀⠀⡴⠁⠐⠆⠱⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠰⡍⠑⠒⠒⢶⡏⠀⣀⣧⣼⣇⠹⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⢠⠇⡇⠀⠀⠀⠀⠻⢹⢿⣻⣿⣿⠟⡟⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠸⠀⠀⠀⠀⠀⡞⠀⡇⠀⠀⠀⠀⠀⢨⣷⣆⠐⣶⠿⡇⠀⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢱⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢘⠃⠀⠃⠀⠀⠀⠀⠀⠘⡗⠒⠒⠒⠒⠁⠀⠈⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    """)
+lines = ["""
+*******************
+Toolkit ::Commands:
+*******************
+1. (toxicdork) SQL Injection Vulnerability scanner(In DEVELOPMENT) (WORKING)
+2. (sqli) SQL Injector (WORKING)
+3. (vulscan) Vulnerability Scanner (WORKING)
+4. (dinfo) Gather Basic Domain Info (WORKING)
+5. (apf) Admin Page Finder (WORKING)
+6. (hashtype) Identify Hash Type (WORKING)
+7. (hexconv) Hex encoder and decoder (WORKING)
+8. (converters) Web Converters (WORKING)
+9. (steghide) Install, Learn and Use Steghide (WORKING)
+10.(exif) Image Metadata Extractor and Remover (WORKING)
+#.(osint) aids in the process of information gathering (NOT WORKING)
+13.(portscan) Scan for Open ports in target system(WORKING)
+14.(localports) Scans for open Local Network Ports(WORKING)
+15.(noobpasswd) Key/Password cracking tools(NOT WORKING)
+16.(toolbox) Extra Set of Tools (NOT WORKING)
+
+******************
+Security Options:
+******************
+1. (spoof my mac) (MAC ADDRESS SPOOF WIFI/ETH) (WORKING)
+2. (itor) install Tor  (WORKING)
+3. (stor) Start Tor  (WORKING)
+4. (tors) Check Tor Status (WORKING)
+6. (vpn) G.U.I VPN Launcher(WORKING) 
+7. (encdns) Encrypt DNS (WORKING)
+8. (anonsearch) Anonymous TOR routed web search(WORKING - NEEDS ATTENTION)
+9. (shells) .PHP Coded Shells/Back-Doors (WORKING - NEEDS ATTENTION)
+10.(phptools) PHP deobfuscators, Testers, beautifiers etc. (WORKING)
+
+(quit) - (home) - (clear) - (update) - (terminal)
+
+~~~2022 EDITION~~~  
+"""]              
+
+from time import sleep
+import sys
+
+for line in lines:          # for each line of text (or each message)
+    for c in line:          # for each character in each line
+        print(c, end='')    # print a single character, and keep the cursor there.
+        sys.stdout.flush()  # flush the buffer
+        sleep(0.003)          # wait a little to make the effect look good.
+    print('')               # line break (optional, could also be part of the message)
